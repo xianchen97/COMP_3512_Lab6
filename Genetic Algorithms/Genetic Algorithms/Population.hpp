cb1b2360db1c3dc1e4dcd324c8645c3f2190f8ad
Xian Chen <xianchen97@gmail.com>
2018-03-09 09:33:44 -0800

    more refactoring

--- Genetic Algorithms/Genetic Algorithms/Population.hpp
+++ Genetic Algorithms/Genetic Algorithms/Population.hpp
@@ -2,12 +2,46 @@
 #include "City.hpp"
 #include "Tour.hpp"
 class Population {
+
+	/* Remember to avoid magic numbers by defining constants */
+	static const int SHUFFLES = 32;  // A shuffle consists of swaps
+
+	static const int  ASCII_OFFSET = 65;    // if we cast an int with value 65
+	// as a char, we get 'A'
+	
+	static const int  ITERATIONS = 1000;   // Number of loops for the algorithm
+
+	static const int  MAP_BOUNDARY = 1000;    // Size of the map (X and Y)
+
+	static const int  FITNESS_SCALER = 10000.0;// Scales the fitness to move the
+	// decimal place to a reasonable spot
+
+	static const int  PARENT_POOL_SIZE = 5; // Number of members randomly selected
+	// from the population, from which the
+	// fittest is made a 'parent'
+
+	static const int  MUTATION_RATE = 0.15; // Chance of being mutated
+
+	static const int  NUMBER_OF_PARENTS = 2;    // Number of 'parents' for each cross
+
+	static const int  NUMBER_OF_ELITES = 1;   // The number of 'fittest' that remain
+	// uncrossed and unmutated into the next gen
+
 	/* Variables */
 	int    i = 0, j = 0, iterations = 0;
 	int    index_of_shortest_tour = 0;
 	double best_distance = 0.0;
 	double best_iteration_distance = 0.0;
 	double improvement_factor = 0.3;
+
+	/* Declares the pointers to our dynamically allocated memory. */
+	static Tour * population ;      // Holds our candidate population
+	static Tour * parents;         // Used during crossover
+	static Tour * crosses;         // Used during calculations
+	static Tour * temporary_tour;  // Used during calculations
+	static City * cities_to_visit; // Stores master list of cities
+	static Tour * child;
+
 
 public:
 
